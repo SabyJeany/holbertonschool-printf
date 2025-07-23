bc15a51eecf047e1d379b803f3c38a9bd339c6e6
root <you@example.com>
2025-07-23 16:16:58 +0000

    m

--- _printf.c
+++ _printf.c
@@ -1,57 +1,68 @@
 #include "main.h"
-#include <stdarg.h>
-#include <unistd.h>
 
 /**
- * _printf - Custom printf function
- * @format: The format string containing format specifiers
+ * _printf - Custom printf function (handles %c, %s, %%)
+ * @format: Format string
  *
  * Return: Number of characters printed
  */
 int _printf(const char *format, ...)
 {
-    va_list args;
-    int i = 0, count = 0;
+	va_list args;
+	int i = 0, count = 0;
+	char *str;
+	char c;
 
-    if (format == NULL)
-        return (-1);
+	if (format == NULL)
+		return (-1);
 
-    va_start(args, format);
+	va_start(args, format);
 
-    while (format[i])
-    {
-        if (format[i] == '%')
-        {
-            i++;
-            if (format[i] == '\0')
-            {
-                write(1, "%", 1);
-                count++;
-                break;
-            }
-            if (format[i] == 'c')
-                count += print_char(args);
-            else if (format[i] == 's')
-                count += print_string(args);
-            else if (format[i] == '%')
-                count += print_percent();
-            else if (format[i] == 'd' || format[i] == 'i')
-                count += print_int(args);
-            else
-            {
-                write(1, "%", 1);
-                write(1, &format[i], 1);
-                count += 2;
-            }
-        }
-        else
-        {
-            write(1, &format[i], 1);
-            count++;
-        }
-        i++;
-    }
+	while (format[i] != '\0')
+	{
+		if (format[i] == '%')
+		{
+			i++;
+			if (format[i] == '\0')
+				break;
+			if (format[i] == 'c')
+			{
+				c = va_arg(args, int);
+				write(1, &c, 1);
+				count++;
+			}
+			else if (format[i] == 's')
+			{
+				str = va_arg(args, char *);
+				if (str == NULL)
+					str = "(null)";
+				while (*str)
+				{
+					write(1, str, 1);
+					str++;
+					count++;
+				}
+			}
+			else if (format[i] == '%')
+			{
+				write(1, "%", 1);
+				count++;
+			}
+			else
+			{
+				write(1, "%", 1);
+				write(1, &format[i], 1);
+				count += 2;
+			}
+		}
+		else
+		{
+			write(1, &format[i], 1);
+			count++;
+		}
+		i++;
+	}
 
-    va_end(args);
-    return (count);
+	va_end(args);
+	return (count);
 }
