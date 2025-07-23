00229c7aca25cdd4723e290b8d28d381760ac788
SabyJeany <Sabyjeany@gmail.com>
2025-07-23 17:32:25 +0200

    m

--- main.c
+++ main.c
@@ -1,5 +1,10 @@
 #include "main.h"
 
+/**
+ * main- Point d'entrée du prograimme, teste la fonction _printf
+ *
+ * Return: 0 si succès
+ */
 int main(void)
 {
 	_printf("Hello %s! You have %d%% chance of success\n", "student", 100);
